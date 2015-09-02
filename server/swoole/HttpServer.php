1760a540e7f00575aec48d4594bde189fa07d715
qiufangzhang <904208360@qq.com>
2015-09-02 22:32:44 +0800

    Update HttpServer.php

--- server/swoole/HttpServer.php
+++ server/swoole/HttpServer.php
@@ -1,19 +1,11 @@
 <?php
-
 class HttpServer
 {
 	public static $instance;
-
 	public $http;
-	public static $get;
-	public static $post;
-	public static $header;
-	public static $server;
 	private $application;
-
 	public function __construct() {
-		$http = new swoole_http_server("127.0.0.1", 9501);
-
+		$http = new swoole_http_server("0.0.0.0", 9501);
 		$http->set(
 			array(
 				'worker_num' => 16,
@@ -22,16 +14,30 @@ class HttpServer
 	            'dispatch_mode' => 1
 			)
 		);
-		$http->on('request',function($request,$response){
-				$response->status('200');
-		 		$response->write(json_encode($request));
-		 		//$response->write('123');
-		 		//$response->end('zqf');
-		});
-
+		$http->on('Request',array($this , 'onRequest'));
 		$http->start();
 	}
 
+	public function onRequest($request,$response) {
+		$response->status('200');
+		$ser=$request->server;
+		$hea= $request->header;
+		$hea['host']=str_replace(':9501','',$hea['host']);//如果端口号是80，就不用要此句代码
+			ob_start();
+			try {
+				define('APPLICATION_PATH', dirname(dirname(__DIR__)). "/application");
+				$this->application = new Yaf_Application(dirname(APPLICATION_PATH). "/conf/application.ini");
+				$yaf_request = new Yaf_Request_Http($ser['request_uri']);
+				$yaf_request->setBaseUri($hea['host']);
+			    $this->application->getDispatcher()->dispatch($yaf_request);
+			} catch ( Yaf_Exception $e ) {
+				//var_dump( $e );
+			}
+			$result = ob_get_contents();
+			ob_end_clean();
+			$response->end($result);
+	}
+	
 	public static function getInstance() {
 		if (!self::$instance) {
             self::$instance = new HttpServer;
