61f101aaf5c95df33ddcee3eaf4216f135e5cbfb
qiufangzhang <904208360@qq.com>
2015-10-23 17:45:53 +0800

    修改

--- application/library/swoole/socket.php
+++ application/library/swoole/socket.php
@@ -1,12 +1,12 @@
 <?php
 class swoole_socket extends Model {
-	private $table="hb_users";
+	private $table="smes_user";
 	public $user;
 	public function __construct() {
 		parent::__construct($this->table);
-		if (!isset($this->user)){  
+		/*if (!isset($this->user)){  
             $this->user = new swoole_socket();   
-        }  
+        }  */
 	}
 	public function getname($userid){
 		//$where=array('id' =>37936);
@@ -16,15 +16,19 @@ class swoole_socket extends Model {
 	public static function savefd($fd){
 		//$where=array('id' =>37936);
 		$redis_con=new phpredis();
+	if($redis_con->listSize('fd')){
 		$redis_con->listPush('fd',$fd,0,1);
+	}else{
+		$redis_con->listPush('fd',$fd);
+	}
 		//file_put_contents( __DIR__ .'/log.txt' , $fd);
 	}
 	public static function getfd(){
 		$redis_con=new phpredis();
-		$result=$redis_con->listGet('fd');
+		$result=$redis_con->listGet('fd',0,-1);
 		 //$m = file_get_contents( __DIR__ .'/log.txt');
 		//echo $m;
-		echo $result;
+		echo json_encode($result);
 	}
 	public static function removefd($fd){
 		$redis_con=new phpredis();
