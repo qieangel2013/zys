7bd2e7ee9cd83d1d8972cc59d8af6a4fcb5b58ae
qiufangzhang <904208360@qq.com>
2016-07-28 14:53:22 +0800

    swoole实现vmstat监控
    swoole实现vmstat监控

--- server/swoole/VmStatServer.php
+++ server/swoole/VmStatServer.php
@@ -6,69 +6,68 @@ class VmStatServer
 	public static $instance;
 	private $application;
 	private $vmstat_handle;
+	private $process;
+	private $server;
 	public function __construct() {
 		define('APPLICATION_PATH', dirname(dirname(__DIR__)). "/application");
 		$this->application = new Yaf_Application(dirname(APPLICATION_PATH). "/conf/application.ini");
 		$this->application->bootstrap();
 
-		$server = new swoole_websocket_server("0.0.0.0", 9502);
+		$this->server = new swoole_websocket_server("0.0.0.0", 9502);
 
-		$server->set(
+		$this->server->set(
 			array(
 				'worker_num' => 1,
 				'daemonize' => true,
 				'log_file' => '/server/log/vmstat.log'
 			)
 		);
+		$this->process = new swoole_process(array(&$this,'vmstata_call'),true);
+		$this->process->name('vmstat监控服务器');
+        $this->process->start();
+		//$this->server->addProcess($this->process);
+		$this->server->on('Start',array(&$this , 'onStart'));
+		$this->server->on('Open',array(&$this , 'onOpen'));
 
-		$server->on('Open',array($this , 'onOpen'));
+		$this->server->on('Message',array(&$this , 'onMessage'));
 
-		$server->on('Message',array($this , 'onMessage'));
+		$this->server->on('Close',array(&$this , 'onClose'));
 
-		$server->on('Close',array($this , 'onClose'));
+		$this->server->start();
+	}
 
-		$server->start();
+	public function onStart($server) {
+			$pro_vs=$this->process;
+			$server=$this->server;
+			$application_vm=$this->application;
+			swoole_event_add($pro_vs->pipe,function($pipe) use($pro_vs,$server,$application_vm){
+				$str = $pro_vs->read();
+				ob_start();
+				$application_vm->execute(array('swoole_socket','getfd'),'vmstat');
+				$result = ob_get_contents();
+				ob_end_clean();
+				$result_fd=json_decode($result,true);
+				foreach($result_fd as $id=>$fd){
+        			$server->push($fd,$str);
+    			}
+			});
+	}
+
+	public function vmstata_call($worker) {
+			$worker->exec('/usr/bin/vmstat', array(1,1000000000));
 	}
 
 	public function onOpen($server, $req) {
 		$this->application->execute(array('swoole_socket','savefd'),$req->fd,'vmstat');
-		$this->vmstat_handle=popen('vmstat 1', 'r');
-
+		$this->server->push($req->fd, "procs -----------memory---------- ---swap-- -----io---- -system-- ----cpu----\n");
+		$this->server->push($req->fd, "r  b   swpd   free   buff  cache   si   so    bi    bo   in   cs us sy id wa\n");
 	}
+
 	public function onMessage($server, $frame) {
-		$framedata=json_decode($frame->data,true);
-    	if('mess'==$framedata['type']){
-    		$server->push($frame->fd,"procs -----------memory---------- ---swap-- -----io---- -system-- ----cpu----\n");
-    		$server->push($frame->fd,"r  b   swpd   free   buff  cache   si   so    bi    bo   in   cs us sy id wa\n");
-			ob_start();
-			$this->application->execute(array('swoole_socket','getfd'),'vmstat');
-			$result = ob_get_contents();
-			ob_end_clean();
-			$result_fd=json_decode($result,true);
-			while(!feof($this->vmstat_handle)) 
-    		{ 
-		    	foreach($result_fd as $id=>$fd){
-        			$server->push($fd,fread($this->vmstat_handle, 1024));
-    	    	}
-    		} 
-    	}else{
-    		$server->Close($frame->fd);
-    	}
-		/*ob_start();
-		$this->application->execute(array('swoole_socket','getfd'));
-		$result = ob_get_contents();
-		ob_end_clean();
-			$result_fd=json_decode($result,true);
-			$line = fread($this->vmstat_handle,512); 
-			print_r($line);
-		    foreach($result_fd as $id=>$fd){
-        		$server->push($fd,$line);
-    	    }*/
     	
 	}
 	public function onClose($server, $fd) {
-		@shell_exec('killall vmstat');
-    	@pclose($this->vmstat_handle);
+		//@shell_exec('killall vmstat');
 		$this->application->execute(array('swoole_socket','removefd'),$fd,'vmstat');
 	}
 	public static function getInstance() {
