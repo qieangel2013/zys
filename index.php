14b7ef3524dc64a9902e0257e68fb2a640ba1f0f
jenchih <393014831@qq.com>
2017-12-28 11:10:08 +0800

    添加了Ctp5组件常量

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 /* INI配置文件支持常量替换 */
 define ("APPLICATION_PATH", __DIR__ . "/application");
+define ("APP_DEBUG", false);
+
 /**
  * 默认的, Yaf_Application将会读取配置文件中在php.ini中设置的ap.environ的配置节
  * 另外在配置文件中, 可以替换PHP的常量, 比如此处的APPLICATION_PATH
